6131bb7ac97d01ea7e1c9b0eed746ff9c77768aa
Umut Topalak <umuttopalak@hotmail.com>
2023-03-30 14:15:44 +0300

    ufak düzeltme

--- frontend/src/pages/LogReg.js
+++ frontend/src/pages/LogReg.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import axios from 'axios';
-import { useState } from 'react';
+import { useState, useAuthUser } from 'react';
 import { useSignIn } from 'react-auth-kit'
 import { useNavigate } from 'react-router-dom';
-
+ 
 
 const LogReg = () => {
 
+    const [showMessage , setMessage] = useState('')
     const [showError, setError] = useState('')
     const [username, setUsername] = useState();
     const [email, setMail] = useState("");
     const [password, setPassword] = useState("");
     const signIn = useSignIn();
     const navigate = useNavigate();
-
 
     const register = (e) => {
         e.preventDefault();
@@ -37,12 +37,15 @@ const LogReg = () => {
         axios(options)
             .then(response => {
                 if (response.status === 201) {
-                    alert("yeyyy")
-
+                    alert("Registered!\nPlease Log In")
+                    setUsername('')
+                    setMail('')
+                    setPassword('')
+                    
                 }
             }).catch((error) => {
                 if (error.response.status === 400) {
-                    alert("o_O")
+                    setMessage("Mail has already been used")
                 }
             })
 
@@ -82,7 +85,7 @@ const LogReg = () => {
                 } else if (axiosError.request) {
                     // İstek gönderilirken bir hata oluştu
                     console.log(axiosError.request);
-                    console.log("abo")
+
                 } else {
                     // Diğer hatalar
                     console.log('Error', axiosError.message);
@@ -97,7 +100,7 @@ const LogReg = () => {
                     <div className="col-12 text-center align-self-center py-5">
                         <div className="section pb-5 pt-5 pt-sm-2 text-center">
                             <h6 className="mb-0 pb-3"><span>Log In </span><span>Sign Up</span></h6>
-                            <input className="checkbox" type="checkbox" id="reg-log" name="reg-log" />
+                            <input className="checkbox" type="checkbox" id="reg-log" name="reg-log" required />
                             <label for="reg-log"></label>
                             <div className="card-3d-wrap mx-auto a">
                                 <div className="card-3d-wrapper">
@@ -107,11 +110,11 @@ const LogReg = () => {
                                                 <div className="section text-center">
                                                     <h4 className="mb-4 pb-3">Log In</h4>
                                                     <div className="form-group">
-                                                        <input value={email} onChange={e => setMail(e.target.value)} type="email" name="email" className="form-style" placeholder="Your Email" id="email" autocomplete="off" />
+                                                        <input value={email} onChange={e => setMail(e.target.value)} type="email" name="email" className="form-style" placeholder="Your Email" id="email" autocomplete="off" required/>
                                                         <i className="input-icon uil uil-at"></i>
                                                     </div>
                                                     <div className="form-group mt-2">
-                                                        <input value={password} onChange={e => setPassword(e.target.value)} type="password" name="password" className="form-style" placeholder="Your Password" id="password" autocomplete="off" />
+                                                        <input value={password} onChange={e => setPassword(e.target.value)} type="password" name="password" className="form-style" placeholder="Your Password" id="password" autocomplete="off" required />
                                                         <i className="input-icon uil uil-lock-alt"></i>
                                                     </div>
                                                     <br />
@@ -127,17 +130,19 @@ const LogReg = () => {
                                                 <div className="section text-center">
                                                     <h4 className="mb-4 pb-3">Sign Up</h4>
                                                     <div className="form-group">
-                                                        <input value={username} onChange={e => setUsername(e.target.value)} type="text" name="username" className="form-style" placeholder="Your Username" id="username" autocomplete="off" />
+                                                        <input value={username} onChange={e => setUsername(e.target.value)} type="text" name="username" className="form-style" placeholder="Your Username" id="username" autocomplete="off" required/>
                                                         <i className="input-icon uil uil-user"></i>
                                                     </div>
                                                     <div className="form-group mt-2">
-                                                        <input value={email} onChange={e => setMail(e.target.value)} type="email" name="email" className="form-style" placeholder="Your Email" id="email" autocomplete="off" />
+                                                        <input value={email} onChange={e => setMail(e.target.value)} type="email" name="email" className="form-style" placeholder="Your Email" id="email" autocomplete="off" required/>
                                                         <i className="input-icon uil uil-at"></i>
                                                     </div>
                                                     <div className="form-group mt-2">
                                                         <input value={password} onChange={e => setPassword(e.target.value)} type="password" name="password" className="form-style" placeholder="Your Password" id="password" autocomplete="off" />
                                                         <i className="input-icon uil uil-lock-alt"></i>
                                                     </div>
+                                                    <br/>
+                                                    {showMessage && <p style={{ color: 'red' }}>{showMessage}</p>}
                                                     <button className='btn mt-4' type='submit' onSubmit={register}>Register</button>
                                                 </div>
                                             </form>
